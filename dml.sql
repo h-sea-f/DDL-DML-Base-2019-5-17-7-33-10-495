c535db0cabeb86ee0d9ddc4c7b4e5893e945376a
h-sea-f <h_sea_f@163.com>
2019-06-23 20:05:04 +0800

    6.23

--- dml.sql
+++ dml.sql
@@ -1,7 +1,9 @@
 -- 插入记录  
-
+insert into student values(003,'贺海峰'，22，'男');
 -- 修改记录  
-
+update student set name='贺' where id=003;
 --  删除记录  
-
+delete from student where id=002;
+	
 -- 查询记录  
+select * from student where id=003;
